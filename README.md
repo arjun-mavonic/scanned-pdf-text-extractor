bb83cd530f793e6a2526914ab2632c32a5df9bd2
Arjun Nayak <arjun@mavonic.com>
2024-06-08 06:38:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# PDF to Docx Converter (non readable pdf, scanned document pdfs)
+
+This is a Python application that converts non-readable PDF files, such as scanned documents, into readable Word documents. It achieves this by first converting the PDF files into images and then extracting the text from the images to create the Word documents. The application provides a user-friendly interface where you can upload PDF files, select the files you want to convert, and initiate the conversion process. Once the conversion is completed, the converted Word documents can be downloaded from the application.
+
+## Installation
+
+1. Clone the repository:
+
+    ```shell
+    git clone https://github.com/arjun-mavonic/scanned-pdf-text-extracter.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```shell
+    cd scanned-pdf-text-extracter
+    ```
+
+3. Create a virtual environment:
+
+    ```shell
+    python -m venv venv
+    ```
+
+4. Activate the virtual environment:
+
+    - For Windows:
+
+      ```shell
+      venv\Scripts\activate
+      ```
+
+    - For macOS/Linux:
+
+      ```shell
+      source venv/bin/activate
+      ```
+
+5. Install the dependencies:
+
+    ```shell
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+1. Run the application:
+
+    ```shell
+    streamlit run main.py
+    ```
+
+2. Upload a PDF file using the file uploader component.
+
+3. Select the PDF files you want to convert to images by checking the checkboxes.
+
+4. Click the "Convert" button to start the conversion process.
+
+5. Once the conversion is completed, the converted Word documents will be available for download in the right column.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
---
+++ utils.py
@@ -0,0 +1,73 @@
+import os
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract
+from docx import Document
+
+
+def create_or_empty_dir(directory):
+    """
+    Create or empty the specified directory.
+
+    Args:
+            directory (str): The directory path.
+    """
+    if os.path.exists(directory):
+        # Empty the directory if it already exists
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            os.remove(file_path)
+    else:
+        # Create the directory if it doesn't exist
+        os.makedirs(directory)
+
+
+def convert_pdf_to_images(input_pdf, output_dir):
+    """
+    Convert a PDF file to a series of images.
+
+    Args:
+            input_pdf (str): The path to the input PDF file.
+            output_dir (str): The directory to save the converted images.
+    """
+    pages = convert_from_path(input_pdf)
+
+    # Save each page as a JPEG file using Pillow
+    for i, page in enumerate(pages):
+        image_path = os.path.join(output_dir, f"page_{i}.jpg")
+        page.save(image_path, "JPEG")
+
+
+def extract_text_from_image(image_path):
+    """
+    Extract text from an image using OCR (Optical Character Recognition).
+
+    Args:
+            image_path (str): The path to the input image file.
+
+    Returns:
+            str: The extracted text from the image.
+    """
+    image = Image.open(image_path)
+    text = pytesseract.image_to_string(image)
+    return text
+
+
+def create_docx_with_text(image_folder, output_docx):
+    """
+    Create a Word document (.docx) with text extracted from images.
+
+    Args:
+            image_folder (str): The directory containing the input images.
+            output_docx (str): The path to save the output Word document.
+    """
+    document = Document()
+    for filename in sorted(
+        os.listdir(image_folder), key=lambda x: int(x.split("_")[1].split(".")[0])
+    ):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            image_path = os.path.join(image_folder, filename)
+            text = extract_text_from_image(image_path)
+            text = text.encode("utf-8", "ignore").decode("latin-1", "ignore")
+            document.add_paragraph(text)
+    document.save(output_docx)
